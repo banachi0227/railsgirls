fe1d0adcc44076f79f11cf2efeb5c63401bb097f
chibana <banachi0227@gmail.com>
2016-12-16 15:27:51 +0900

    kadai サインアップ機能(新規登録の実行)の実装〜２章終わり

--- kadai/config/application.rb
+++ kadai/config/application.rb
@@ -2,7 +2,6 @@ require_relative 'boot'
 
 require 'rails/all'
 
-@import bootstrap
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -13,6 +12,14 @@ module Kadai
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
+    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
+    # config.time_zone = 'Central Time (US & Canada)'
+
+    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+
     config.sass.preferred_syntax = :sass
   end
 end
--- kadai/config/routes.rb
+++ kadai/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 resource :registrations, only: [:new, :create]
 
+
+  root to: 'registrations#new'
+
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

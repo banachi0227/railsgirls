6658b8e5180e5a67e5dc4498ba246bbe831ac131
chibana <banachi0227@gmail.com>
2016-12-18 14:49:31 +0900

    kadai 4章

--- kadai/config/routes.rb
+++ kadai/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
 resource :registrations, only: [:new, :create]
 resource :sessions, only: [:new, :create, :destroy]
+resources :users, only: [:index, :show]
 
   root to: 'registrations#new'
 
